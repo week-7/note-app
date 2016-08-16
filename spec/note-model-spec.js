3d06b5fb5e43c5d0564547a8a682544e62dab949
Aga Kowalczuk <kowalczuk.a@outlook.com>
2016-08-16 11:56:08 +0100

    list model and some testing

--- spec/note-model-spec.js
+++ spec/note-model-spec.js
@@ -1,3 +1,6 @@
+var Note = require('../src/note-model').note;
+var LocalStorage = require('../src/localStorage').localStorage;
+
 
 (function testContentOnInstantiation() {
   var note = new Note("Hey");
@@ -10,8 +13,9 @@
 
 (function savingContent() {
   var note = new Note("Hey");
-  note.saveNote();
-  var retrievedObject = localStorage.getItem('noteContent');
+  var storage = new LocalStorage();
+  note.saveNote(storage);
+  var retrievedObject = storage.getItem('noteContent');
   if (retrievedObject !== "Hey") {
     throw new Error("Text of the note was not saved");
   } else {
--- src/note-model.js
+++ src/note-model.js
@@ -4,14 +4,19 @@
 
   function Note(noteContent) {
     this.noteContent = noteContent;
-    }
+  }
 
-  Note.prototype.saveNote = function() {
-    localStorage.setItem('noteContent', this.noteContent);
-  };
+  // Note.prototype.saveNote = function(localStorage) {
+  //   localStorage.setItem('noteContent', this.noteContent);
+  // };
 
   Note.prototype.readNote = function() {
-    return localStorage.getItem('noteContent');
+    return this.noteContent;
   };
+  //
+  // Note.prototype.getNote = function() {
+  //   return localStorage.getItem('noteContent');
+  // };
+
   exports.note = Note;
 })(this);
