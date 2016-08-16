24e682992e7b91515657fe0d7a0cf14a796e307e
Aga Kowalczuk <kowalczuk.a@outlook.com>
2016-08-16 14:21:14 +0100

    re-wrote modules into a revealing module pattern

--- src/note-list-view-model.js
+++ src/note-list-view-model.js
@@ -1,18 +1,32 @@
 
 
-(function(exports) {
+var View = (function() {
 
-  function View(list) {}
+  return {
+      HTMLstring: function() {
+        output = '<ul>';
+        var notelist = List.readListContent();
+        notelist.forEach(function(note) {
+          output += '<li>' + note + '</li>';
+        });
+        output += '</ul>';
+        return output;
+      }
+    };
 
-  View.prototype.returnHTMLString = function() {
-    output = '<ul>';
-    list.listContent.forEach(function(note) {
-      output += '<li>' + note.noteContent + '</li>';
-    });
-    output += '</ul>';
-    return output;
-  };
 
-  exports.view = View;
+  })();
 
-})(this)
+//   View.prototype.returnHTMLString = function() {
+//     output = '<ul>';
+//     var list = List.readListContent();
+//     list.forEach(function() {
+//       output += '<li>' + Note.printNote() + '</li>';
+//     });
+//     output += '</ul>';
+//     return output;
+//   };
+//
+//   exports.view = View;
+//
+// })(this)
