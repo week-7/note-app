e44b58f35239f7443659ba00db0c1020472e67b3
NJBow <nicholas.j.bowman@hotmail.com>
2016-08-17 14:00:21 +0100

    save button functionality

--- src/note-list-view-model.js
+++ src/note-list-view-model.js
@@ -3,12 +3,17 @@
 
     return {
         noteList: function() {
+          var elem = document.getElementById('list');
+          while (elem.hasChildNodes())
+            elem.removeChild(elem.firstChild);
           var list = List.readListContent();
           for (var i = 0; i < list.length; i++) {
-            var newList = document.createElement('li');
-            var liText = document.createTextNode(list[i]);
-            newList.appendChild(liText);
-            document.getElementById('list').appendChild(newList);
+            var listLink = document.createElement('a');
+            var listDiv = document.createElement('div');
+            var listText = document.createTextNode(list[i]);
+            listLink.appendChild(listDiv);
+            listDiv.appendChild(listText);
+            document.getElementById('list').appendChild(listLink);
           }
         }
       };
